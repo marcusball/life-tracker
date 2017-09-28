b81d223141cf578d0a96c701c5d57d128b97bc72
Marcus Ball <marcus@marcusball.me>
2017-09-28 01:21:20 -0400

    model and schema declarations

---
+++ src/models.rs
@@ -0,0 +1,17 @@
+use chrono::datetime::DateTime;
+use chrono::offset::utc::UTC;
+
+#[derive(Queryable)]
+pub struct Counter {
+    pub id: i32,
+    pub name: String,
+    pub unit: String,
+}
+
+#[derive(Queryable)]
+pub struct CounterEvent {
+    pub id: i32,
+    pub cid: i32,
+    pub quantity: i32,
+    pub timestamp: DateTime<UTC>,
+}
