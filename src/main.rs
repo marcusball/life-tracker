74c576561e5b5a489178777d120b30c34bd172ef
Marcus Ball <marcus@marcusball.me>
2017-09-28 02:13:14 -0400

    dummy main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+#![feature(plugin, custom_derive)]
+#![plugin(rocket_codegen)]
+
+extern crate chrono;
+#[macro_use]
+extern crate diesel;
+#[macro_use]
+extern crate diesel_codegen;
+extern crate dotenv;
+extern crate env_logger;
+#[macro_use]
+extern crate lazy_static;
+#[macro_use(log, info, debug, trace, warn)]
+extern crate log;
+extern crate r2d2;
+extern crate r2d2_diesel;
+extern crate rocket;
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
+
+pub mod schema;
+pub mod models;
+mod database;
+
+use database::DbConn;
+
+#[get("/")]
+fn hello(conn: DbConn) -> &'static str {
+    let _ = conn.get();
+
+    "Suh, dude"
+}
+
 fn main() {
-    println!("Hello, world!");
+    rocket::ignite()
+        .manage(database::init_pool())
+        .mount("/", routes![hello])
+        .launch();
 }
