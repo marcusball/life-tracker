b4b7dc141e62c96036203a2bc5e461991bd5f00f
Marcus Ball <marcus@marcusball.me>
2017-10-19 01:03:24 -0400

    load counters via js (and log them)

--- static/js/pages/index.js
+++ static/js/pages/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     console.log("A'ight, DOM is loaded, where's the SUB? ;)");
+
+    loadCounters();
 });
+
+function loadCounters() {
+    fetch('/counters')
+        .then((response) => { return response.json(); })
+        .then((counters) => { console.log(counters); });
+}
