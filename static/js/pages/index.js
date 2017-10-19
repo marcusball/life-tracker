1520ddb19f4c31a77a406f2fdbbda5cfe0fff728
Marcus Ball <marcus@marcusball.me>
2017-10-19 01:45:22 -0400

    display a basic html list of counters

--- static/js/pages/index.js
+++ static/js/pages/index.js
@@ -1,11 +1,37 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     console.log("A'ight, DOM is loaded, where's the SUB? ;)");
 
-    loadCounters();
+    displayCounters();
 });
 
+function displayCounters() {
+    Promise.all([loadCounters(), compileCounterTemplate()])
+        .then((promises) => {
+            let counters = promises[0];
+            let template = promises[1];
+
+            let html = template({ counters: counters });
+
+            let counterContainer = document.getElementById('js-counters');
+            counterContainer.insertAdjacentHTML('beforeend', html);
+        });
+}
+
+/**
+ * Loads an array of counters. 
+ * @returns Promise a Promise of an array of Counter objects. 
+ */
 function loadCounters() {
-    fetch('/counters')
-        .then((response) => { return response.json(); })
-        .then((counters) => { console.log(counters); });
+    return fetch('/counters')
+        .then((response) => { return response.json(); });
+}
+
+/**
+ * Loads the template for displaying a list of counters. 
+ * @returns Promise a Promise of a compiled Handlebars template. 
+ */
+function compileCounterTemplate() {
+    return fetch('/static/templates/index-counters.html.hbs')
+        .then((response) => { return response.text(); })
+        .then(Handlebars.compile);
 }
