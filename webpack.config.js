4b20638e38c3bac6d2896b11f8ebecc5082610ea
Marcus Ball <marcus@marcusball.me>
2017-12-02 21:01:15 -0500

    lets webpack some shit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const glob = require('glob')
+
+const srcroot = './srcjs/';
+
+module.exports = {
+    entry: glob.sync(srcroot + "**/*.ts").reduce((entries, entry) =>
+        Object.assign(entries, {
+            [entry.replace(srcroot, '').replace('.ts', '')]: entry
+        }),
+        {}),
+    output: {
+        path: __dirname + '/static/js',
+        filename: '[name].bundle.js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    }
+};
