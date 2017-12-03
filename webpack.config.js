3450ccbbef6a534f5768d728a32c8fd9591c7540
Marcus Ball <marcus@marcusball.me>
2017-12-02 21:09:53 -0500

    generate source map output

--- webpack.config.js
+++ webpack.config.js
@@ -14,13 +14,17 @@ module.exports = {
         filename: '[name].bundle.js',
         publicPath: '/'
     },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
                 use: 'ts-loader',
                 exclude: /node_modules/
-            }
+            },
+            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
         ]
     },
     resolve: {
